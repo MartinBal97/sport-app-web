fdcaf917d690de80bb9027b7e4ac6228dd21bb58
MartinBal97 <tinchoabalverde@gmail.com>
2024-12-04 12:16:55 +0100

    changing the proyect for using typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-export default function App() {
+function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -22,4 +23,4 @@ export default function App() {
   );
 }
 
-
+export default App;
